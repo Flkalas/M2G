5ea4f534802c19654fe8035b8804913b8df70d83
Flkalas <rectifying@gmail.com>
2015-10-03 20:00:21 +0900

    v1.1.3
    Update 1.1.3
    - Stable multiple download enabled with task queue.

--- src/main.js
+++ src/main.js
@@ -3,14 +3,40 @@
 // found in the LICENSE file.
 var worker;
 var nameFile;
+var running = false;
 var flag = false;
-var delay = 100;
-var widthVideo;
-var heightVideo;
+var delay = 80;
+var playbackRate = 2.0;
 var canvas = document.createElement('canvas');
 var context = canvas.getContext('2d');
+var queue = [];
 
-function getDownloadLink(fileData) {
+var createVideoElement;
+
+function processNextTask(){
+	if(queue.length < 1){
+		running = false;
+		console.log("Worker All Jobs Done");
+		return running;
+	}
+	
+	console.log("Now Left Task: ",queue.length);
+	var src = queue.shift();
+	createVideoElement(src);
+	return true;	
+}
+
+function activate(){
+	if(running){
+		return false;
+	}
+	
+	console.log("Worker Activates.");
+	running = true;
+	processNextTask();
+}
+
+function getDownloadLink(fileData){
 	var blob = new Blob([fileData]);
 	chrome.downloads.download({url: window.URL.createObjectURL(blob), filename: nameFile+".gif" },function(id){});
 }
@@ -20,35 +46,32 @@ function initWorker() {
 	worker.onmessage = function (event) {
 		console.log("Process Ended");
 		var u8Array = new Uint8Array(atob(event.data).split("").map(function(c){return c.charCodeAt(0); }));
-		getDownloadLink(u8Array);		
+		getDownloadLink(u8Array);
+		processNextTask();
 	};
 }
 
 function draw(v,c,w,h) {
 	if(v.paused || v.ended)	return false;
 	c.drawImage(v,0,0,w,h);
-	if(flag == true){	
+	if(flag == true){			
 		var imdata = c.getImageData(0,0,w,h);		
-		worker.postMessage({frame: imdata});
+		worker.postMessage({frame: imdata});		
 	}
-	setTimeout(draw,delay,v,c,w,h);
+	setTimeout(draw,delay/playbackRate,v,c,w,h);
 }
 
-// A generic onclick callback function.
-function genericOnClick(info) {
-	console.log(info.srcUrl);
-	nameFile = info.srcUrl.substring(info.srcUrl.lastIndexOf('/')+1).split(".")[0];
+createVideoElement = function(src){
+	nameFile = src.substring(src.lastIndexOf('/')+1).split(".")[0];
 	
 	var eleVideo = document.createElement('video');
 	
 	eleVideo.addEventListener('loadeddata', function(){
 		console.log("First frame loaded");
-		draw(this,context,widthVideo,heightVideo);
+		draw(this,context,this.videoWidth,this.videoHeight);
 	}, false );
 	eleVideo.addEventListener("loadedmetadata", function (e) {
-		console.log("Metadata loaded");
-		widthVideo = this.videoWidth;
-		heightVideo = this.videoHeight;		
+		console.log("Metadata loaded");		
 		eleVideo.play();
 	}, false );
 	eleVideo.addEventListener("ended", function (e) {
@@ -56,18 +79,25 @@ function genericOnClick(info) {
 		console.log("Play ended");
 		worker.postMessage({});
 	}, false );
-	eleVideo.addEventListener('play', function(){
+	eleVideo.addEventListener('play', function(){		
 		console.log("Play start");
-		canvas.width = widthVideo;
-		canvas.height = heightVideo;
+		canvas.width = this.videoWidth;
+		canvas.height = this.videoHeight;
+		worker.postMessage({delay:delay,w:this.videoWidth,h:this.videoHeight});
 		flag = true;
-		worker.postMessage({delay:delay,w:widthVideo,h:heightVideo});	
 	},false);
 	
-	eleVideo.src = info.srcUrl;
-	eleVideo.playbackRate = 1.0;
+	eleVideo.src = src;
+	eleVideo.playbackRate = playbackRate;
 	eleVideo.preload = "metadata";
-	eleVideo.innerHTML = '<source src="' + eleVideo.src + '" type="video/mp4 preload="metadata" />';
+	eleVideo.innerHTML = '<source src="' + eleVideo.src + '" type="video/mp4 preload="metadata" />';	
+};
+
+// A generic onclick callback function.
+function genericOnClick(info) {
+	console.log(info.srcUrl);
+	queue.push(info.srcUrl);	
+	activate();
 }
 
 // Create one test item for each context type.
